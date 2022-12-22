114ec1b0df9dbad92975ba0fdf9d848f11f18245
Cheng Che <clementcheng56@gmail.com>
2022-12-23 00:13:46 +0800

    feat: 100 Same Tree

--- src/__tests__/100_same_tree.spec.ts
+++ src/__tests__/100_same_tree.spec.ts
@@ -3,6 +3,33 @@ import { isSameTree } from '../same-tree';
 
 describe('is the given tree are the same', () => {
   it('truthy if the trees are null', () => {
-    expect(isSameTree(null, null)).toBe(true);
+    expect(isSameTree()).toBe(true);
+  });
+
+  it('falsy if the trees in different shape', () => {
+    expect(
+      isSameTree(
+        { val: 1, left: { val: 2 }, right: undefined },
+        { val: 1, left: undefined, right: { val: 3 } }
+      )
+    ).toBe(false);
+  });
+
+  it('falsy if the value of tree nodes are different', () => {
+    expect(
+      isSameTree(
+        { val: 1, left: { val: 2 }, right: { val: 3 } },
+        { val: 1, left: { val: 3 }, right: { val: 2 } }
+      )
+    ).toBe(false);
+  });
+
+  it('truthy if the tree are the same', () => {
+    expect(
+      isSameTree(
+        { val: 1, left: { val: 2 }, right: { val: 3 } },
+        { val: 1, left: { val: 2 }, right: { val: 3 } }
+      )
+    ).toBe(true);
   });
 });
--- src/same-tree.ts
+++ src/same-tree.ts
@@ -1,8 +1,23 @@
 import type TreeNode from './tree-node';
 
-function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+/**
+ * # question:
+ *  - is a null tree equals another null tree? `YES`
+ *
+ * # thinking:
+ *  - the edge case: two null tree should be the same
+ *  - consider the value of the tree nodes are the same, of course, check null first.
+ *  - if the value of the tree nodes are the same, then check the equality between the left children and the right children.
+ *
+ * # complexity:
+ *  - time:  O(n)
+ *  - space: O(1)
+ */
+function isSameTree(p?: TreeNode | null, q?: TreeNode | null): boolean {
   if (!p && !q) return true;
-  return false;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
 
 export { isSameTree };
